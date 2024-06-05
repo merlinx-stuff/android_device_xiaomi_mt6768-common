8ee1eacb69e98da23b966f822f6dee6f1c183e58
SamarV-121 <samar@samarv121.dev>
2024-06-05 08:59:01 +0530

    mt6768-common: Use libmtkperf_client from hardware/mediatek
    It's back again **polish jerry face**
    
    Change-Id: If3c651d0411be4440259cc8c760b4776873d54c2

--- mt6768.mk
+++ mt6768.mk
@@ -386,6 +386,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/mediatek \
+    hardware/mediatek/libmtkperf_client \
     $(LOCAL_PATH)
 
 # Thermal
