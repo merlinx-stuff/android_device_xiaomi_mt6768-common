455aba9e2ade25cd235103de2dd882bb36c9c57b
bengri32 <bengris32@protonmail.ch>
2024-06-17 20:01:41 +0530

    mt6768-common: Include `hardware/xiaomi` Soong namespace

--- mt6768.mk
+++ mt6768.mk
@@ -385,6 +385,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/mediatek \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek/libmtkperf_client \
+    hardware/xiaomi \
     $(LOCAL_PATH)
 
 # Thermal
