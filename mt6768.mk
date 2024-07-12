c67c9a75863447a5cd8c8177b9bcce5592a9c07a
Bruno Martins <bgcngm@gmail.com>
2024-07-12 19:50:10 +0530

    mt6768-common: Patch libs for missing GraphicBufferMapper symbols
    Change-Id: I8d02058ae97cb027fd36ee2362dcffb8443cd5c8

--- mt6768.mk
+++ mt6768.mk
@@ -379,7 +379,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_audio \
     libshim_beanpod \
-    libpiex_shim
+    libpiex_shim \
+    libui_shim
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
