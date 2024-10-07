6188cefb5cae6b0c2481ba2550e69cbedd76658c
Ryuichi12 <seanchendana@gmail.com>
2024-10-07 17:51:23 +0700

    mt6768-common: Build more VNDK blobs
    Signed-off-by: Ryuichi12 <seanchendana@gmail.com>

--- mt6768.mk
+++ mt6768.mk
@@ -121,7 +121,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libcurl.vendor \
-    libexpat.vendor
+    libexpat.vendor \
+    libgralloctypes.vendor
 
 # Disable SF configstore
 PRODUCT_PACKAGES += \
@@ -141,6 +142,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdrm \
+    libui.vendor \
+    libion.vendor \
     libdrm.vendor
 
 # fastbootd
@@ -163,6 +166,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
+
+PRODUCT_PACKAGES += \
+    libgatekeeper.vendor
 
 # GNSS
 PRODUCT_PACKAGES += \
@@ -393,6 +399,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libshim_sensors \
+    libpower.vendor \
     libsensorndkbridge
 
 # Shims
