125e4a42b84173727658319fd9d727b77e41da3e
Woomymy <woomy@woomy.be>
2024-10-29 15:23:15 +0100

    yunluo: Initial skeleton tree
    Change-Id: I847cc342cf51bf0e9061bb7c3015772800a2612e
    Signed-off-by: Woomymy <woomy@woomy.be>

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_yunluo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_yunluo-user \
+    lineage_yunluo-userdebug \
+    lineage_yunluo-eng
