05c92a8648daf446ed6794570662ee84ac607b6a
Woomymy <woomy@woomy.be>
2024-10-30 12:23:34 +0100

    yunluo: Fixup graphics allocator service to load service from `mt6789/`
    Change-Id: I858150b1532f59ea7ae1a73ad151ec7e50551086
    Signed-off-by: Woomymy <woomy@woomy.be>

--- extract-files.py
+++ extract-files.py
@@ -4,9 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.fixups_blob import (
-    blob_fixups_user_type,
-)
+from extract_utils.fixups_blob import blob_fixups_user_type, blob_fixup
 from extract_utils.fixups_lib import (
     lib_fixup_remove_arch_suffix,
     lib_fixup_remove_proto_version_suffix,
@@ -22,7 +20,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/yunluo',
+    "device/xiaomi/yunluo",
 ]
 
 
@@ -34,17 +32,21 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
+    "vendor/etc/init/android.hardware.graphics.allocator@4.0-service-mediatek.rc": blob_fixup().regex_replace(
+        "android.hardware.graphics.allocator@4.0-service-mediatek",
+        "mt6789/android.hardware.graphics.allocator@4.0-service-mediatek.mt6789",
+    ),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'yunluo',
-    'xiaomi',
+    "yunluo",
+    "xiaomi",
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
     check_elf=True,
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     utils = ExtractUtils.device(module)
     utils.run()
