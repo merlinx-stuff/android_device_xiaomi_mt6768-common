a3fbfed9c3643d9a0e836d713efe71182dfd2c9d
Michael Bestas <mkbestas@lineageos.org>
2024-11-05 17:35:28 +0700

    mt6768-common: Shim widevine with libcrypto_shim The following changes google/boringssl@e202e51 removed symbols that our libs require.
    * also revert 6616862560d547ff690cfdc1e1f4edc417969a83
    and sha1sum
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    
    Signed-off-by: Ryuichi12 <seanchendana@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -105,6 +105,10 @@ function blob_fixup {
         system_ext/lib64/libsource.so)
             grep -q libui_shim.so "$2" || "$PATCHELF" --add-needed libui_shim.so "$2"
             ;;
+        vendor/lib*/libwvhidl.so | vendor/lib*/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
     esac
 }
 
