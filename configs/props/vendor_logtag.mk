a072bf1d7cdd29ab2ef05e2f51894ffb3c6122ea
hipexscape <abhinav.115260@gmail.com>
2024-11-13 13:30:52 +0530

    mt6768-common: Silence GPUAUX logspam
    Change-Id: I31f5b462a080fcaf3fdc1da36be8ab2a65695e88

--- configs/props/vendor_logtag.mk
+++ configs/props/vendor_logtag.mk
@@ -279,5 +279,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.log.tag.MAPI-CommandProcessor=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.RpAudioControl=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.MDP=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL)
 
