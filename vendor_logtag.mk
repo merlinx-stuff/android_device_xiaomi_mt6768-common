ebb1a1afce5134c5e3093626d2eb120ca4c68fc1
seanarv1n <seanchendana@gmail.com>
2024-12-03 22:18:23 +0700

    mt6768-common: Silent Profiler logspam

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -289,5 +289,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.MDP=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.android.hardware.sensors@2.0-service-mediatek=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.Acceleration=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.Profiler=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL)
 
